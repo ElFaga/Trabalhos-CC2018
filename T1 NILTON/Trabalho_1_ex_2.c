a5c3b0d75680828f16ef03852b0f267d87268b45
Lucas Roberto Raineri Oliveira <lucasrobertoliveira@hotmail.com>
2018-05-19 23:48:48 -0400

    Final_ver
    Final

--- T1 NILTON/Trabalho_1_ex_2.c
+++ T1 NILTON/Trabalho_1_ex_2.c
@@ -1,4 +1,4 @@
-/*Por 
+/*Por
       George Andre  (38338)
       Lincoln Lins  (38344)
       Lucas Roberto (38346)
@@ -10,28 +10,28 @@ int main()
 {
     int p1, p2, c1, c2, a, b;
     //Valida o primeiro peso informado
-    printf("Escreva o peso da primeira crianca:");
+    printf("Escreva o peso da primeira crianca(em Kg):");
     scanf("%i", &p1);
     while (p1<10 || p1>100){
         printf("\nNumero invalido. Informe novamente.");
         scanf("%i", &p1);
     }
     //Valida o comprimento do lado da gangorra em que a primeira pessoa sentará
-    printf("\nAgora o comprimento do lado aonde a crianca vai sentar:");
+    printf("\nAgora o comprimento do lado aonde a crianca vai sentar(em cm):");
     scanf("%i", &c1);
     while (c1<10 || c1>100){
         printf("\nNumero invalido. Informe novamente.");
-        scanf("%i", &p2);
+        scanf("%i", &c1);
     }
     //Valida o segundo peso informado
-    printf("\nEscreva o peso da segunda crianca:");
+    printf("\nEscreva o peso da segunda crianca(em Kg):");
     scanf("%i", &p2);
     while (p2<10 || p2>100){
         printf("\nNumero invalido. Informe novamente.");
-        scanf("%i", &c1);
+        scanf("%i", &p2);
     }
     //Valida o comprimento do lodo da gangorra em que a segunda pessoa sentará
-    printf("\nE o comprimento do lado aonde a crianca vai sentar:");
+    printf("\nE o comprimento do lado aonde a crianca vai sentar(em cm):");
     scanf("%i", &c2);
     while (c2<10 || c2>100){
         printf("\nNumero invalido. Informe novamente.");
@@ -41,13 +41,13 @@ int main()
     a = p1*c1;
     b = p2*c2;
     if (a==b){
-        printf("\n0");
+        printf("\n0\n");
     }
     if (a>b){
-        printf("\n-1");
+        printf("\n-1\n");
     }
     if (a<b){
-        printf("\n1");
+        printf("\n1\n");
     }
     system("pause");
     return 0;
